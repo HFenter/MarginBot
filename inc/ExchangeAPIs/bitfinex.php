503b8eae703e9679a4e9dbd6bea1ea26ef6fb001
Anthony Dang <danghtanthony@gmail.com>
2017-10-14 17:12:04 -0600

    Fixed 30 day loan lending out as 2 days when calculated rate was below minimum rate

--- inc/ExchangeAPIs/bitfinex.php
+++ inc/ExchangeAPIs/bitfinex.php
@@ -399,12 +399,13 @@ class Bitfinex{
 							// NEW VERSION: ( 0.00365 / annual = 0.00001 / day )
 							$loans[$cur][$a]['rate'] = ( ($l['rate'] - 0.00365) > $minLendRateAnnual ? ($l['rate'] - 0.00365) : $minLendRateAnnual ) ;
 							//echo $loans[$a]['rate'].'<br>';
+							$lrate=$loans[$cur][$a]['rate']+0.00365;
 							
 							//how long should we lend this out... as a rule, 2 days so we can cycle and get a high turnover
 							// unless its above the threshold $this->actSettings['thirtyDayMin'], in which case we should lend it for the max 30 days
 							// (basically, the rate is higher than normal, lets keep this loan out as long as possible)
 							//  if $this->actSettings['thirtyDayMin'] = 0, always loan for 2 days, no matter what
-							$loans[$cur][$a]['time'] = (($this->actSettings['thirtyDayMin'][$cur]>0)&&($l['rate'] > ($this->actSettings['thirtyDayMin'][$cur] * 365)) ? 30 : 2);
+							$loans[$cur][$a]['time'] = (($this->actSettings['thirtyDayMin'][$cur]>0)&&($lrate > ($this->actSettings['thirtyDayMin'][$cur] * 365)) ? 30 : 2);
 							$nextlend += $gapClimb;
 							$a++;
 						}
